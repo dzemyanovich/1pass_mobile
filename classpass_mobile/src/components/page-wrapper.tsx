b66d6c671ea5814e9150c9ab65a9ffe8d603dfad
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-11-28 17:22:45 +0300

    fix a few bug for Android

--- classpass_mobile/src/components/page-wrapper.tsx
+++ classpass_mobile/src/components/page-wrapper.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { KeyboardAvoidingView, ScrollView, StyleSheet, View } from 'react-native';
 import { hasDynamicIsland, hasNotch } from 'react-native-device-info';
 
-import { iOS } from '../utils/utils';
+import { Android, iOS } from '../utils/utils';
 
 export default function PageWrapper({ children, isNavigationTab = false, alignCenter = true }: PageWrapperProps) {
   const margin = 10;
   const marginTop = isNavigationTab && iOS() && (hasDynamicIsland() || hasNotch())
     ? 60
     : margin;
-  const paddingBottom = isNavigationTab
-    ? 60
-    : 0;
+
+  let paddingBottom;
+  if (!isNavigationTab) {
+    paddingBottom = 0;
+  } else {
+    paddingBottom = Android()
+      ? 15
+      : 60;
+  }
 
   const marginStyles = {
     marginTop,
@@ -37,13 +43,21 @@ export default function PageWrapper({ children, isNavigationTab = false, alignCe
     },
   });
 
-  return (
-    <KeyboardAvoidingView style={styles.keyboardView} behavior="padding" enabled keyboardVerticalOffset={100}>
+  return Android()
+    ? (
       <ScrollView contentContainerStyle={alignCenter ? styles.centerContainer : styles.container}>
         <View>
           {children}
         </View>
       </ScrollView>
-    </KeyboardAvoidingView>
-  );
+    )
+    : (
+      <KeyboardAvoidingView style={styles.keyboardView} behavior="padding" enabled keyboardVerticalOffset={100}>
+        <ScrollView contentContainerStyle={alignCenter ? styles.centerContainer : styles.container}>
+          <View>
+            {children}
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    );
 }
